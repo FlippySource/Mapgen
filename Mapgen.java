62bf3522d25d7af36cc541d6bc95e0fbc81aecc5
Philippe Bareau <Bareau.Philippe@gmail.com>
2017-12-24 20:12:43 -0500

    Implemented merge and bridge builder, still fine tuning values

--- Mapgen.java
+++ Mapgen.java
@@ -6,26 +6,42 @@ import java.util.Random;
 import javax.imageio.ImageIO;
 
 public class Mapgen {
+	private Random rand = new Random();
+	private int [][] board;
 	public static void main(String[] args) {
 
 		Mapgen mapgen = new Mapgen();
 		int boardx = 1000;
 		int boardy = 1000;
-		int[][] board = new int[boardx][boardy]; // 0 is empty else floor #
+		mapgen.board = new int[boardx][boardy]; // 0 is empty else floor #
 		int min = 5;
 		int max = 10;
 		Random random = new Random();
 		int roomcount = random.nextInt((max - min) + 1) + min;
 		int pixelfills = (boardx * boardy) / 5;
 
-		board = mapgen.recursiveBranchingFill(board, boardx, boardy, roomcount, pixelfills, mapgen, random);
-		board = mapgen.smoothBoard(board);
+		mapgen.recursiveBranchingFill(boardx, boardy, roomcount, pixelfills, mapgen, random);
+		
+		/*//and again...
+		Mapgen mapgen2 = new Mapgen();
+		mapgen2.board = new int[boardx][boardy]; // 0 is empty else floor #
+		mapgen2.recursiveBranchingFill(boardx, boardy, roomcount, pixelfills, mapgen, random);
+		mapgen.merge(mapgen2.board);
+		*/
+		
+		mapgen.bridgeBuilder(6, 50);
+		mapgen.smoothBoard();
 
-		mapgen.makeImage(boardx, boardy, board);
+		mapgen.makeImage(boardx, boardy);
 
 	}
-
-	public BufferedImage makeImage(int boardx, int boardy, int[][] board) {
+	
+	public int[][] getBoard()
+	{
+		return board;
+	}
+	
+	private BufferedImage makeImage(int boardx, int boardy) {
 
 		BufferedImage tempimage = new BufferedImage(boardx + 1, boardy + 1, BufferedImage.TYPE_3BYTE_BGR);
 		for (int x = 0; x < boardx; x++) {
@@ -49,7 +65,7 @@ public class Mapgen {
 		return tempimage;
 	}
 
-	public int[][] randomFill(int[][] board, int boardx, int boardy, int roomcount, int pixelfills, Random random) {
+	private void randomFill(int boardx, int boardy, int roomcount, int pixelfills, Random random) {
 		for (int x = 0; x < boardx; x++) {
 			for (int y = 0; y < boardy; y++) {
 				board[x][y] = 0;
@@ -65,27 +81,25 @@ public class Mapgen {
 				i++;
 			}
 		}
-		return board;
 	}
 
-	public int[][] recursiveBranchingFill(int[][] board, int boardx, int boardy, int roomcount, int pixelfills,
+	private void recursiveBranchingFill(int boardx, int boardy, int roomcount, int pixelfills,
 			Mapgen mapgen, Random random) { // fill 1 random, recurse once for each created pixel with x chance of
 											// adjacent
 		int pixelfill = pixelfills;
-		int[][] bd = board;
 
 		while (pixelfill > 0) {
 			int pickx = random.nextInt(boardx - 1);
 			int picky = random.nextInt(boardy - 1);
-			if (bd[pickx][picky] == 0) {
-				bd[pickx][picky] = 1;
+			if (board[pickx][picky] == 0) {
+				board[pickx][picky] = 1;
 			}
 
-			ArrayList<Pixel> picks = recurseFill(bd, boardx, boardy, random, pickx, picky, pixelfill, 0);
+			ArrayList<Pixel> picks = recurseFill(boardx, boardy, random, pickx, picky, pixelfill, 0);
 
 			for (int i = 0; i < picks.size(); i++) {
 				Pixel temp = (Pixel) picks.get(i);
-				bd[temp.x][temp.y] = temp.val;
+				board[temp.x][temp.y] = temp.val;
 
 			}
 
@@ -93,14 +107,11 @@ public class Mapgen {
 			picks = new ArrayList<Pixel>();
 		}
 
-		return bd;
-
 	}
 
-	public ArrayList<Pixel> recurseFill(int[][] bd, int boardx, int boardy, Random rand, int px, int py, int pixelfills,
+	private ArrayList<Pixel> recurseFill(int boardx, int boardy, Random rand, int px, int py, int pixelfills,
 			int depth) {// something is exiting its recursion early and not handing back up some
 						// information
-		int[][] board = bd;
 		if (depth > 1200) {
 			// makeImage(boardx, boardy, board);
 			return (new ArrayList<Pixel>());
@@ -145,7 +156,7 @@ public class Mapgen {
 					return out;
 				}
 
-				out.addAll(recurseFill(board, boardx, boardy, rand, nx, ny, pixelfills - out.size(), depth + 1));
+				out.addAll(recurseFill(boardx, boardy, rand, nx, ny, pixelfills - out.size(), depth + 1));
 
 				if (pixelfills - out.size() <= 0) {
 					return out;
@@ -156,7 +167,7 @@ public class Mapgen {
 
 					board[temp.x][temp.y] = temp.val;
 				}
-				out.addAll(recurseFill(board, boardx, boardy, rand, px, py, pixelfills - out.size(), depth + 1));
+				out.addAll(recurseFill(boardx, boardy, rand, px, py, pixelfills - out.size(), depth + 1));
 
 				return out;
 
@@ -171,7 +182,7 @@ public class Mapgen {
 		return (new ArrayList<Pixel>());
 	}
 
-	public int[][] smoothBoard(int[][] board) {
+	private void smoothBoard() {
 
 		int changes = 1;
 		while (changes > 0) {
@@ -203,32 +214,90 @@ public class Mapgen {
 				}
 			}
 		}
-
-		return board;
 	}
 
-	public int[][] bridgeBuilder(int[][] board, int bridgeWidth, int bridgeLength) {
+	private void bridgeBuilder(int bridgeWidth, int bridgeLength) {
 		int xlen = board.length;
 		int ylen = board [0].length;
 		int curlen = bridgeLength;
+		int curwid = bridgeWidth;
 		//horizontal first
-		for (int x = 0; x < board.length; x++) {
-			
-		
-			for (int y = 0; y < board[x].length; y++) {
-				curlen = (y + bridgeLength >= ylen)? bridgeLength : (ylen - y - 1);
-				for (int i = 0; i < curlen; i++)
-				if (board[x][y] + i == 1)//fill one pixel if opp then for each width do it again even if redundant
+		for (int x = 0; x < board.length - 2; x++) 
+		{
+			curlen = (x + bridgeLength >= xlen - 1)? (xlen - (x + 1)) :  bridgeLength;
+			for (int y = 0; y < board[x].length - 2; y++) {
+				if (board [x][y] == 1 && board [x + 1][y] + board[x + 2][y] == 0)
 				{
-					
+					curwid = (y + bridgeWidth >= ylen - 1)?  (ylen - (y + 1)) : bridgeWidth;
+					for (int i = 2; i < curlen; i++)
+					{
+						if (board[x + i][y] == 1)//fill one pixel if opp then for each width do it again even if redundant
+						{
+							if(rand.nextInt(100) == 0)
+							{
+								bridge( x, y, curwid, curlen, 'H');
+							}
+							break;
+						}
+					}
 				}
-					
-				
-	
+			}
+		} // horizontal done
+		//vert next
+		for (int x = 0; x < board.length - 2; x++) 
+		{
+			curwid = (x + bridgeWidth >= xlen - 1)? (xlen - (x + 1)) : bridgeWidth;
+			for (int y = 0; y < board[x].length - 2; y++) {
+				if (board [x][y] == 1 && board [x][y + 1] + board[x][y + 2] == 0)
+				{
+					curlen = (y + bridgeLength >= ylen - 1)? (ylen - (y + 1)) : bridgeLength;
+					for (int i = 2; i < curlen; i++)
+					{
+						if (board[x][y + i] == 1)//fill one pixel if opp then for each width do it again even if redundant
+						{
+							if(rand.nextInt(100) == 0) //maybe relate chance to length
+							{
+								bridge(x, y, curwid, curlen, 'V');
+							}
+							break;
+						}
+					}
+				}
 			}
 		}
-			return board;
 		
 	}
+	
+	private void bridge(int x, int y, int curwid, int curlen, char dir)
+	{
+	
+		for( int i = 0; i < curlen; i++)
+		{
+			for( int j = 0; j < curwid; j++)
+			{
+				if( dir == 'H') //h for horizontal, probably v for vertical
+				{
+					board [x + i][ y + j] = 1;
+				}
+				else
+				{
+					board [x + j][ y + i] = 1;
+				}
+			}
+		}
+	}
 
+	private void merge(int[][] board2)
+	{
+		for (int i = 0; i < board.length; i++)
+		{
+			for (int j = 0; j < board[i].length; j++)
+			{
+				if(board[i][j] == 1 || board2[i][j] == 1)
+				{
+					board[i][j] = 1;
+				}
+			}
+		}
+	}
 }
